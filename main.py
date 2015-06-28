aa82dd2ec2c4f5d9f58a84e7e4de38c9029c4136
Ryan M <rjames86@gmail.com>
2015-06-28 16:16:03 -0700

    fix for running script from another path

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import pkgutil
 import os
 import datetime
 
-# path = os.path.join(os.path.dirname('.'), "enabled")
 path = os.path.join(os.path.dirname(__file__), "enabled")
+base_path =  os.path.basename(path)
 modules = pkgutil.iter_modules(path=[path])
 
 print datetime.datetime.now(), "running..."
 
 for loader, mod_name, ispkg in modules:
-    loaded_mod = __import__(path + "." + mod_name, fromlist=[mod_name])
+    loaded_mod = __import__(base_path + "." + mod_name, fromlist=[mod_name])
     print "Loading", mod_name
     loaded_mod.main()
